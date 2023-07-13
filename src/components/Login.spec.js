737bf71a9fc7070517ef6d3ae4f27da6f739defd
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-07-13 16:18:17 +0530

    added login test cases

--- src/components/Login.spec.js
+++ src/components/Login.spec.js
@@ -1,10 +1,74 @@
 import { expect, test } from "vitest";
 import { shallowMount } from "@vue/test-utils";
 import Login from "./Login.vue";
-const wrapper = shallowMount(Login);
+
+global.alert = () => {};
+
+//First Test case-I
 test("Test successfully!", () => {
+  const wrapper = shallowMount(Login);
   expect(wrapper.vm.userData).toEqual({
     email: "",
     password: "",
   });
+});
+
+//Second Test case-II
+
+test("Submit form with valid data", async () => {
+  const mockStore = {
+    getters: {
+      getUserData: {
+        email: "",
+        password: "",
+      },
+    },
+  };
+  const wrapper = shallowMount(Login, {
+    global: {
+      mocks: {
+        $store: mockStore,
+      },
+    },
+  });
+  await wrapper.setData({
+    "userData.email": "shubhamtribhuvan@gmail.com",
+    "userData.password": "password123",
+  });
+  wrapper.vm.isSubmitted = false;
+  await wrapper.vm.submit();
+  expect(wrapper.vm.isSubmitted).toBe(false);
+  expect(wrapper.vm.formValid).toBe(undefined);
+});
+
+//Third Test case-III
+test("Routing after successful form submission", async () => {
+  const mockStore = {
+    getters: {
+      getUserData: {
+        email: "test@example.com",
+        password: "password123",
+      },
+    },
+  };
+  const mockRouter = {
+    push: function (route) {
+      this.pushedRoute = route;
+    },
+    pushedRoute: "/post",
+  };
+  const wrapper = shallowMount(Login, {
+    global: {
+      mocks: {
+        $store: mockStore,
+        $router: mockRouter,
+      },
+    },
+  });
+  await wrapper.setData({
+    "userData.email": "shubhamtribhuvan@example.com",
+    "userData.password": "password123",
+  });
+  await wrapper.vm.submit();
+  expect(mockRouter.pushedRoute).toBe("/post");
 });
